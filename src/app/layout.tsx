54846c3f0ca0985b85da30b733c87e0d688f9688
rizalgamatecha <rizal@team.gamatecha.com>
2025-12-12 14:02:41 +0700

    feat: add meta for seo optimazion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,43 @@ export const metadata: Metadata = {
   title: 'OmniSource - One Platform to Manage All Your Outsourcing Operations',
   description:
     'OmniSource adalah platform SaaS berbasis web dan mobile yang dirancang untuk mengelola operasional outsourcing secara terintegrasi, transparan, dan efisien.',
+  openGraph: {
+    title:
+      'OmniSource - One Platform to Manage All Your Outsourcing Operations',
+    description:
+      'OmniSource adalah platform SaaS berbasis web dan mobile yang dirancang untuk mengelola operasional outsourcing secara terintegrasi, transparan, dan efisien.',
+    type: 'website',
+    url: `${process.env.NEXT_PUBLIC_LP_URL}`,
+    siteName: 'OmniSource',
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_LP_URL}/assets/white-logo.webp`,
+        width: 1200,
+        height: 630,
+        alt: 'OmniSource - One Platform to Manage All Your Outsourcing Operations',
+      },
+    ],
+  },
+  twitter: {
+    title:
+      'OmniSource - One Platform to Manage All Your Outsourcing Operations',
+    description:
+      'OmniSource adalah platform SaaS berbasis web dan mobile yang dirancang untuk mengelola operasional outsourcing secara terintegrasi, transparan, dan efisien.',
+    card: 'summary_large_image',
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_LP_URL}/assets/white-logo.webp`,
+        width: 1200,
+        height: 630,
+        alt: 'OmniSource - One Platform to Manage All Your Outsourcing Operations',
+      },
+    ],
+  },
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/favicon-32x32.webp',
+    shortcut: '/favicon-32x32.webp',
+  },
 };
 
 const rubik = Rubik({
