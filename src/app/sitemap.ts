3b84acce564b1a82893a4a9e716a5d0d33ee9d5c
rizalgamatecha <rizal@team.gamatecha.com>
2025-12-18 09:51:31 +0700

    fix: Correct sitemap URL paths by adding missing slashes.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,19 +9,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_LP_URL}terms`,
+      url: `${process.env.NEXT_PUBLIC_LP_URL}/terms`,
       lastModified: new Date(),
       changeFrequency: 'never',
       priority: 0.8,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_LP_URL}privacy-policy'`,
+      url: `${process.env.NEXT_PUBLIC_LP_URL}/privacy-policy'`,
       lastModified: new Date(),
       changeFrequency: 'never',
       priority: 0.5,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_LP_URL}data-security'`,
+      url: `${process.env.NEXT_PUBLIC_LP_URL}/data-security'`,
       lastModified: new Date(),
       changeFrequency: 'never',
       priority: 0.5,
